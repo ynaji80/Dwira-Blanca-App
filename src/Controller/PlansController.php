aefd2c5a91caa435c8b0a1588388a6a0fc8e6c08
ynaji80 <najiyounes80@gamil.com>
2022-05-05 10:08:54 +0000

    Added plans page

--- src/Controller/PlansController.php
+++ src/Controller/PlansController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class PlansController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/plans', name: 'app_plans')]
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('plans.html.twig');
     }
 }
